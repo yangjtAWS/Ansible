9983d5ad3c721af808f0cf5e4d77070cf462bcb3
Jingtao Yang <jingtaoyang@Jingtaos-MacBook-Pro.local>
2018-02-20 23:54:10 -0800

    new new world

--- roles/helloworld/files/helloworld.js
+++ roles/helloworld/files/helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
 
    // Send the response body as "Hello World"
-   response.end('Hello old World again\n');
+   response.end('Hello new new World\n');
 }).listen(3000);
 
 // Console will print the message
